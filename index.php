874cefc579601a07ad99b620784a341d90af4d1f
RidwanRamadhan612 <57112222+RidwanRamadhan612@users.noreply.github.com>
2019-12-23 11:30:38 +0700

    test

---
+++ index.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="fontawesome/fontawesome/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Buruh Jogja Serikat</title>
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-lg navbar-light mt-2 my-3" style="background-color: black;">
+            <img src="img/logosbsi.png" alt="logo" style="width: 150px; height: 100px;" class="ml-3">
+            <div class="container justify-content-center mt-2">
+              <h4 class="float-center text-primary"><span>Serikat Buruh Seluruh Indonesia</span><h4>
+            </div>
+        </nav>
+    
+        <section>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-light mt-2">
+                <ul class="nav nav-pills mr-auto ml-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sejarah/index.html">Sejarah</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="visimisi/index.html">Visi & Misi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="berita/index.html">Berita</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mars/index.html">Mars Buruh</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Galeri</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="galeri/foto.html">foto</a>
+                           <a class="dropdown-item" href="galeri/video.html">Video</a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+    
+            <div class="bd-example mt-3">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="img/foto.png" class="d-block w-100" alt="foto" style="height: 400px;">
+                            <div class="carousel-caption d-none d-md-block">
+                                
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/foto1.png" class="d-block w-100" alt="foto" style="height: 400px;">
+                            <div class="carousel-caption d-none d-md-block">
+                               
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/foto2.png" class="d-block w-100" alt="foto" style="height: 400px;">
+                            <div class="carousel-caption d-none d-md-block">
+                                
+                            </div>
+                    </div>
+                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container marketing mt-5">
+            <div class="row text-center">
+                <div class="col-lg-4">
+                    <img src="img/logo2.png" alt="logo" class="img" style="width: 130px; height: 130px;">
+                    <h4>Visi & Misi</h4>
+                        <p> Berdasarkan tujuan berdirinya organisasi sebagaimana tertuang dalam Anggaran Dasar Pasal 8 :</p>
+                        <p><a class="btn btn-primary" href="visimisi/index.html" role="button">Read More &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="img/logo3.png" alt="logo" class="img" style="width: 130px; height: 130px;">
+                    <h4>Sejarah</h4>
+                        <p> SBSI adalah Serikat Buruh Sejahtera Indonesia. Pada masa rezim diktator Suharto hanya mengijinkan satu wadah serikat buruh.</p>
+                        <p><a class="btn btn-primary" href="sejarah/index.html" role="button">Read More &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="img/logo1.png" alt="logo" class="img" style="width: 130px; height: 130px;">
+                   <h4>Mars Buruh</h4>
+                    <p> Dalam Wadah yang Mandiri
+                        Kita bersatu memacu diri
+                        Singsingkan lengan menerpa rintangan
+                        Dengan menggalang kebersamaan</p>
+                    <p><a class="btn btn-primary" href="mars/index.html" role="button">Read More &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+
+                <hr style="border: 2px solid grey;" class="mt-4 mb-4">
+                <div class="text text-center">
+                    <h2 class="text-center">Tentang SBSI</h2>
+                </div>
+                    <div class="media">
+                        <img src="img/logosbsi.png" class="align-self-start mr-2" alt="buruh Jogja serikat" style="width: 200px; height: 150px;">
+                        <div class="media-body">
+                          <p>SBSI adalah Serikat Buruh Sejahtera Indonesia. Pada masa rezim diktator Suharto hanya mengijinkan satu wadah serikat buruh. Serikat-serikat buruh independen yang sebelumnya lahir pada masa Orde Lama di bawah pimpinan presiden Sukarno, dipaksa unifikasi ke SPSI (Serikat Pekerja Seluruh Indonesia) oleh Menteri Tenaga Kerja eks-militer Sudomo. Unifikasi ini dilakukan pada tahun 1985. Sebelumnya dimulai dengan unifikasi dalam wadah berbentuk federasi tahun 1972 dalam FSPSI, namun dirubah lagi menjadi unitaris tahun 1985 dalam wadah SPSI. Sejak fusi yang dipaksakan itu, SPSI berubah total menjadi mesin politik Orde Baru, banyak pensiunan tentara menjadi pengurus SPSI di daerah. Serikat pekerja dijadikan organ pemerintah dalam bentuk “state corportism”. Inilah awal yang membuat buruh kecewa terhadap SPSI.</p>
+                        </div>
+                    </div>
+        </div>
+    </section>
+
+        <div class="footer mt-5">
+        <div class="container-fluid" style="background-color: black;">
+            <div class="row">
+                  <div class="col-12 col-md mt-4 ml-3">
+                        <img src="img/logosbsi.png" alt="" style="width: 200px; height: 150px;">
+                    <small class="d-block mb-3 text-muted">&copy;2019</small>
+                  </div>
+                  <div class="col-4 col-s mt-4">
+                    <h5>Buruh Jogja Serikat</h5>
+                    <ul class="list-unstyled text-small ">
+                      <li class="text-muted">Lokasi: <p>Jl. Parangtritis km.12</p></li>
+                    </ul>
+                  </div>
+                  <div class="col-4 col-xs mt-2">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                      <li><a class="text-muted" href="#">Team</a></li>
+                      <li><a class="text-muted" href="#">Locations</a></li>
+                      <li><a class="text-muted" href="https://www.Instagram.com/rid.wanr/">Instagram</a></li>
+                      <li><a class="text-muted" href="https://wa.me/6285702050672">Whatsapps</a></li>
+                    </ul>
+                  </div>
+            </div>
+        </div>
+        </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
